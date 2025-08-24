fa2518cc8f8dba0a129c7a568c4f14aa47d6d43f
Sitti Ainnie Dimalna <sitti@praxxys.ph>
2025-08-24 20:21:50 +0800

    deploy

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,23 +1,31 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2024-04-03',
-  devtools: { enabled: false },
-  css: ['~/assets/css/main.css'],
-  modules: ['@nuxtjs/tailwindcss', '@nuxtjs/google-fonts'],
-  googleFonts: {
-    families: {
-      Roboto: true,
-      'Josefin+Sans': true,
-      Lato: [100, 300],
-      Raleway: {
-        wght: [100, 400],
-        ital: [100]
-      },
-      'Plus Jakarta Sans': '200..800',
-      'Crimson Pro': {
-        wght: '200..900',
-        ital: '200..700',
-      }
-    }
-  }
-})
+    compatibilityDate: "2024-04-03",
+    devtools: { enabled: false },
+    ssr: true, // still required for static generation
+    target: "static",
+    app: {
+        baseURL: "/benline-vehicle-insurance/", // important: match your repo name
+    },
+    nitro: {
+        preset: "github_pages",
+    },
+    css: ["~/assets/css/main.css"],
+    modules: ["@nuxtjs/tailwindcss", "@nuxtjs/google-fonts"],
+    googleFonts: {
+        families: {
+            Roboto: true,
+            "Josefin+Sans": true,
+            Lato: [100, 300],
+            Raleway: {
+                wght: [100, 400],
+                ital: [100],
+            },
+            "Plus Jakarta Sans": "200..800",
+            "Crimson Pro": {
+                wght: "200..900",
+                ital: "200..700",
+            },
+        },
+    },
+});
